e1991209174b3847b655fc1f59eabe89fff36834
Ngazdii <azdialafkar@gmail.com>
2026-09-15 14:07:43 +0700

    belajar

---
+++ biodata_app.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+window = tk.Tk()
+window.title("Form Biodata Mahasiswa")
+window.geometry("500x600")
+window.resizable(False, False)
+window.configure(bg="skyblue")
+window.mainloop()
